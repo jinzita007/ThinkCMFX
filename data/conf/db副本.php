be34172965bcb20b6cb558afbe96ec7da37f093c
zhou <jinzita007@yahoo.com>
2016-06-08 15:33:13 +0800

    0.0.1

---
+++ data/conf/db副本.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cmf_',
+    //密钥
+    "AUTHCODE" => 'DH8Wzn5kYlWwwV0tNu',
+    //cookies
+    "COOKIE_PREFIX" => '9jppI3_',
+);
